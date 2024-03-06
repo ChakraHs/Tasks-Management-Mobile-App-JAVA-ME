c6169490528960242c8302f611af1697a469e4d0
hussein <hussien.chakra@gmail.com>
2024-03-06 20:29:37 +0100

    add signUp and connect firebase

---
+++ app/src/main/java/com/example/mytask/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.mytask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,SignUpActivity.class));
+                finish();
+            }
+        },1000);
+    }
+}
