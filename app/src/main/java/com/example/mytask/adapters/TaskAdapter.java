8b1c9a6798901ba024beb680fd895c75ac108b59
Chakra Hs <lahoucine_chakra@um5.ac.ma>
2024-03-10 16:21:54 +0100

    add tasks recycler view

--- app/src/main/java/com/example/mytask/adapters/TaskAdapter.java
+++ app/src/main/java/com/example/mytask/adapters/TaskAdapter.java
@@ -1,5 +1,7 @@
 package com.example.mytask.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -8,31 +10,31 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mytask.R;
+import com.example.mytask.Utility;
 import com.example.mytask.models.Task;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 public class TaskAdapter extends FirestoreRecyclerAdapter<Task, TaskAdapter.TaskViewHolder> {
 
-    /**
-     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
-     * FirestoreRecyclerOptions} for configuration options.
-     *
-     * @param options
-     */
-    public TaskAdapter(@NonNull FirestoreRecyclerOptions<Task> options) {
+    Context context;
+    public TaskAdapter(@NonNull FirestoreRecyclerOptions<Task> options, Context context) {
         super(options);
+        this.context=context;
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull TaskViewHolder holder, int position, @NonNull Task model) {
-
+    protected void onBindViewHolder(@NonNull TaskViewHolder holder, int position, @NonNull Task task) {
+        holder.titleTextView.setText(task.getTitle());
+        holder.descriptionTextView.setText(task.getDescription());
+        holder.timestampTextView.setText(Utility.timestampToString(task.getTimestamp()));
     }
 
     @NonNull
     @Override
     public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_task_item,parent,false);
+        return new TaskViewHolder(view);
     }
 
     class TaskViewHolder extends RecyclerView.ViewHolder{
