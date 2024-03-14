07255307476ef9e3ebac3c5ef553c59140074455
Chakra Hs <lahoucine_chakra@um5.ac.ma>
2024-03-14 02:16:22 +0100

    add profile page

--- app/src/main/java/com/example/mytask/ProfileActivity.java
+++ app/src/main/java/com/example/mytask/ProfileActivity.java
@@ -2,13 +2,33 @@ package com.example.mytask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+    //Set on click listner on image view to open uploadprofilrpicactivity
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageView = findViewById(R.id.profile_image);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this,UploadProfilePicActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
